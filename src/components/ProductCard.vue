2bc4274d9580a7a1264296e7a37960de9c48c5c5
haku19602 <haku19602@gmail.com>
2024-02-18 23:59:47 +0800

    前台商店頁面切板

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <VCard elevation="0" rounded="xl">
+    <RouterLink :to="'/products/' + _id">
+      <VImg :src="image" cover height="400"></VImg>
+    </RouterLink>
+    <VCardText class="d-flex justify-space-between">
+      {{ name }}
+      <VChip density="comfortable">{{ category }}</VChip>
+    </VCardText>
+    <VCardSubtitle>NT.{{ price }}</VCardSubtitle>
+    <v-divider class="mx-3 mt-3"></v-divider>
+    <VCardActions>
+      <VBtn prepend-icon="mdi-cart" color="primary" rounded size="small" @click="addCart">加入購物車</VBtn>
+      <v-spacer></v-spacer>
+      <VBtn icon="mdi-heart-outline" color="secondary" @click="addLike"></VBtn>
+    </VCardActions>
+
+  </VCard>
+<!--
+VCard.product-card
+  VImg(:src="image" cover height="200")
+  VCardTitle
+    RouterLink.text-primary.text-decoration-none(:to="'/products/' + _id") {{ name }}
+  VCardSubtitle ${{ price }}
+  //- white-space: pre -> 顯示換行
+  VCardText(style="white-space: pre;") {{ description }}
+  VCardActions
+    VBtn(color="primary" prepend-icon="mdi-cart" @click="addCart") 加入購物車
+-->
+</template>
+
+<script setup>
+// import { useApi } from '@/composables/axios'
+// import { useUserStore } from '@/store/user'
+// import { useSnackbar } from 'vuetify-use-dialog'
+// import { useRouter } from 'vue-router'
+
+// const { apiAuth } = useApi()
+// const user = useUserStore()
+// const createSnackbar = useSnackbar()
+// const router = useRouter()
+
+const props = defineProps(['_id', 'category', 'description', 'image', 'name', 'price', 'sell', 'stock'])
+
+// const addCart = async () => {
+//   if (!user.isLogin) {
+//     router.push('/login')
+//     return
+//   }
+//   try {
+//     const { data } = await apiAuth.patch('/users/cart', {
+//       product: props._id,
+//       quantity: 1
+//     })
+//     user.cart = data.result
+//     createSnackbar({
+//       text: '新增成功',
+//       showCloseButton: false,
+//       snackbarProps: {
+//         timeout: 2000,
+//         color: 'green',
+//         location: 'bottom'
+//       }
+//     })
+//   } catch (error) {
+//     const text = error?.response?.data?.message || '發生錯誤，請稍後再試'
+//     createSnackbar({
+//       text,
+//       showCloseButton: false,
+//       snackbarProps: {
+//         timeout: 2000,
+//         color: 'red',
+//         location: 'bottom'
+//       }
+//     })
+//   }
+// }
+</script>
--- src/views/front/ShopView.vue
+++ src/views/front/ShopView.vue
@@ -1,6 +1,62 @@
 <template>
-  <h1>shop 商店</h1>
+  <VContainer>
+    <VRow>
+      <VCol cols="12">
+        <h2>商品</h2>
+      </VCol>
+
+      <VCol cols="12" md="6" lg="4" v-for="product in products" :key="product._id">
+        <!-- 商品卡片寫成元件傳入 -->
+        <ProductCard v-bind="product"></ProductCard>
+      </VCol>
+    </VRow>
+  </VContainer>
 </template>
+
+<script setup>
+import { ref, onMounted, nextTick } from 'vue'
+import { useApi } from '@/composables/axios'
+import { useSnackbar } from 'vuetify-use-dialog'
+import ProductCard from '@/components/ProductCard.vue'
+import gsap from 'gsap'
+
+const { api } = useApi()
+const createSnackbar = useSnackbar()
+
+// ===== 商品陣列
+const products = ref([])
+
+// ===== 取得商品資料
+onMounted(async () => { // 不用 onMounted 的話，就是在 created 的生命週期呼叫 api，那裡面就不能對 DOM 做處理
+  try {
+    // === 元件被掛到 DOM 時，才會發請求去後端要資料
+    const { data } = await api.get('/products', {
+      params: {
+        itemsPerPage: -1 // 沒有分頁 - 回全部??????????
+      }
+    })
+    // === 把要到的資料 push 進 products 陣列
+    products.value.push(...data.result.data)
+    // === 等待 vue 重新渲染頁面
+    await nextTick() // data 更新後不會立即渲染 DOM
+    // === gsap 動畫
+    // gsap
+    //   .to('.product-card', { opacity: 1, duration: 0.5 })
+  } catch (error) {
+    console.log(error)
+    const text = error?.response?.data?.message || '發生錯誤，請稍後再試'
+    createSnackbar({
+      text,
+      showCloseButton: false,
+      snackbarProps: {
+        timeout: 2000,
+        color: 'secondary',
+        location: 'bottom'
+      }
+    })
+  }
+})
+</script>
 
 <style lang="scss" scoped>
 </style>
