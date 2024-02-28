f775aa27ad4f95b675ad085b645e2c4502c223b6
haku19602 <145089039+haku19602@users.noreply.github.com>
2024-02-28 20:26:55 +0000

    Deploying to gh-pages from @ haku19602/beetlefactory_front@dda342be7661b985b29a8e3521cae2daf9b54bac 🚀

---
+++ assets/BookAdult-697cf035.js
@@ -0,0 +1,1 @@
+import{_ as e,Q as c,R as o}from"./index-312f7d63.js";const t={};function n(r,s){return c(),o("h1",null,"成蟲")}const a=e(t,[["render",n]]);export{a as default};
--- assets/HomeView-19cf20dd.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,Q as o,R as t,Z as s}from"./index-724c5dd8.js";const c={},n={style:{background:"#F8F4EB",height:"100%"}},_=s("h1",null,"admin home",-1),a=[_];function r(d,i){return o(),t("div",n,a)}const l=e(c,[["render",r]]);export{l as default};
