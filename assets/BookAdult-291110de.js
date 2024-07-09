1144681cb35853220d6850dd9a204b73373c8852
haku19602 <145089039+haku19602@users.noreply.github.com>
2024-07-09 16:47:54 +0000

    Deploying to gh-pages from @ haku19602/beetlefactory_front@6f9619c89d11e2fb6505e928278e0d452b5d3f99 🚀

---
+++ assets/BookAdult-291110de.js
@@ -0,0 +1,1 @@
+import{_ as e,Q as c,R as o}from"./index-3a588185.js";const t={};function n(r,s){return c(),o("h1",null,"成蟲")}const a=e(t,[["render",n]]);export{a as default};
--- assets/BookBaby-3686feb9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,Q as c,R as o}from"./index-b2c8fafa.js";const n={};function r(t,a){return c(),o("h1",null,"幼蟲")}const _=e(n,[["render",r]]);export{_ as default};
